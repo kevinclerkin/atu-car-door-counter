d47f055503b9a12e81d7efad17f12da04821dd70
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-11-10 22:40:06 +0000

    refactor(door) refactor door class as separate class (#11)

---
+++ Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDoorCounter
+{
+    public class Car
+    {
+        private readonly int _numberOfDoors = 2;
+
+        public int NumberOfDoors
+        {
+            get { return _numberOfDoors; }
+        }
+
+        public bool GetNumberOfDoors()
+        {
+            return NumberOfDoors > 1 && NumberOfDoors < 5;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,18 +5,5 @@
         Console.WriteLine("Welcome to the Car Door Counter!");
     }
 
-    public class Car
-    {
-        private readonly int _numberOfDoors = 2;
-
-        public int NumberOfDoors
-        {
-            get { return _numberOfDoors; }
-        }
-
-        public bool GetNumberOfDoors()
-        {
-            return NumberOfDoors > 1 && NumberOfDoors < 5;
-        }
-    }
+    
 }
